d019ecb6b1f922bf1944f7c503d721f0ad8e582f
rdz <rdz@pop-os.localdomain>
2023-09-24 11:33:05 +0200

    add class object & styles

--- snapface/src/app/face-snap/face-snap.component.ts
+++ snapface/src/app/face-snap/face-snap.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-face-snap',
   templateUrl: './face-snap.component.html',
   styleUrls: ['./face-snap.component.scss']
 })
-export class FaceSnapComponent {
+export class FaceSnapComponent implements OnInit {
+title!: string;
+description!: string;
+createdDate!: Date;
+snaps!: number;
+imageURL!: string;
 
+ngOnInit(): void {
+  this.title = "Archibald";
+  this.imageURL = "https://animalaxy.fr/wp-content/uploads/2019/01/iStock-1067074306-758x505.jpg";
+  this.description = "Mon meilleur ami depuis toujours";
+  this.createdDate = new Date();
+  this.snaps = 6;
+}
 }
