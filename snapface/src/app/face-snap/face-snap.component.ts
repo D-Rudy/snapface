3b98a3b7f85e4fc6d5a6bd9b1c09d8a0f865a55f
D-Rudy <duriez.ry@gmail.com>
2023-09-24 12:07:14 +0200

    décrement

--- snapface/src/app/face-snap/face-snap.component.ts
+++ snapface/src/app/face-snap/face-snap.component.ts
@@ -6,17 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./face-snap.component.scss']
 })
 export class FaceSnapComponent implements OnInit {
-title!: string;
-description!: string;
-createdDate!: Date;
-snaps!: number;
-imageURL!: string;
+  title!: string;
+  description!: string;
+  createdDate!: Date;
+  snaps!: number;
+  imageURL!: string;
+  bouton!: string;
 
-ngOnInit(): void {
-  this.title = "Archibald";
-  this.imageURL = "https://animalaxy.fr/wp-content/uploads/2019/01/iStock-1067074306-758x505.jpg";
-  this.description = "Mon meilleur ami depuis toujours";
-  this.createdDate = new Date();
-  this.snaps = 6;
-}
+  ngOnInit(): void {
+    this.title = "Archibald";
+    this.imageURL = "https://animalaxy.fr/wp-content/uploads/2019/01/iStock-1067074306-758x505.jpg";
+    this.description = "Mon meilleur ami depuis toujours";
+    this.createdDate = new Date();
+    this.snaps = 6;
+    this.bouton = "Snap!";
+  }
+
+  onSnap() {
+    if (this.bouton == "Snap!") {
+      this.snaps++;
+      this.bouton = "Oops... déSnap!";
+    }
+    else {
+      this.snaps--;
+      this.bouton = "Snap!";
+    }
+  }
 }
